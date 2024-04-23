bd064c1e21d97ad821cd01c5c77ab95031fb7586
GabImoV <163057198+GabImoV@users.noreply.github.com>
2024-04-22 21:04:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 window.onload = function() {
-    var audio = document.getElementById("background-audio");
-    var button = document.getElementById("healing-button");
-    var videoContent = document.getElementById("video-content");
-    var prelude = document.getElementById("prelude");
-  
-    button.onclick = function() {
-      audio.pause(); // Pausa a música ao clicar no botão
-      prelude.style.display = "none"; // Esconde o conteúdo de introdução
-      videoContent.style.display = "block"; // Mostra o conteúdo do vídeo
-    };
+  var audio = document.getElementById("background-audio");
+  var button = document.getElementById("healing-button");
+  var videoContent = document.getElementById("video-content");
+  var prelude = document.getElementById("prelude");
+
+  button.onclick = function() {
+    audio.pause(); // Pausa a música ao clicar no botão
+    prelude.style.display = "none"; // Esconde o conteúdo de introdução
+    videoContent.style.display = "block"; // Mostra o conteúdo do vídeo
   };
-  
+
+  // Add code to play the audio automatically here (e.g., after checking if loaded)
+  // audio.play(); // Uncomment this if you want autoplay
+};
