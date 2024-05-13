37e987fd5e5d6b2300848fcdc5563475517eb483
GabImoV <163057198+GabImoV@users.noreply.github.com>
2024-05-12 21:09:59 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function calcularProrata() {
-    var valorTotal = parseFloat(document.getElementById('valorTotal').value.replace(",", "."));
-    var quantidadeDias = parseFloat(document.getElementById('quantidadeDias').value.replace(",", "."));
-    var valorMulta = parseFloat(document.getElementById('valorMulta').value.replace(",", "."));
-
-    var resultado = ((valorTotal / 30) * quantidadeDias) + valorMulta;
-
-    document.getElementById('resultado').innerText = "Resultado: R$ " + resultado.toLocaleString('pt-BR', {minimumFractionDigits: 2});
-}
